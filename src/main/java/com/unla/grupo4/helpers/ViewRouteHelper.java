8277b6ea5216e9f50f93e2647ed494a1e58ad87e
Eduardo Melgarejo <edumelgarejo27@gmail.com>
2023-06-19 15:17:26 -0300

    Proyecto base

---
+++ src/main/java/com/unla/grupo4/helpers/ViewRouteHelper.java
@@ -0,0 +1,13 @@
+package com.unla.grupo4.helpers;
+
+public class ViewRouteHelper {
+	/**** Views ****/
+	//User
+	public final static String USER_SELECT = "user/user_select";
+	public final static String USER_ADMIN = "user/admin";
+	public final static String USER_AUDITOR = "user/auditor";
+	
+	/**** Redirects ****/
+	public final static String ROUTE = "/user/user_select";
+	
+}
