b16b4384252dc3be5780f7861754be165904dab9
Eduardo Melgarejo <edumelgarejo27@gmail.com>
2023-06-23 00:47:37 -0300

    actualizado ruteos y añadido index para ver todos los eventos

--- src/main/java/com/unla/grupo4/helpers/ViewRouteHelper.java
+++ src/main/java/com/unla/grupo4/helpers/ViewRouteHelper.java
@@ -7,7 +7,21 @@ public class ViewRouteHelper {
 	public final static String USER_ADMIN = "user/admin";
 	public final static String USER_AUDITOR = "user/auditor";
 	
+	//Dispositivo
+	public final static String DISPOSITIVO_INDEX = "/dispositivo";
+	
+	//Evento
+	public final static String EVENTO_INDEX= "eventos/index";
+	
+	//SensorBasura
+	public final static String SENSORBASURA_NUEVO = "/sensor_basura/nuevo";
+	public final static String SENSORBASURA_INDEX = "/sensor_basura/index";
+	public final static String SENSORBASURA_UPDATE = "sensor_basura/update";
+	
 	/**** Redirects ****/
 	public final static String ROUTE = "/user/user_select";
+	public final static String SENSORBASURA_ROOT ="/sensor_basura";
+	public static final String EVENTOS_ROOT = "/eventos";
+	
 	
 }
