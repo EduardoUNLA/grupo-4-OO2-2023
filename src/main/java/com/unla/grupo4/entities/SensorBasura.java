92b700474343840f04d20cd913c48517d63bb290
Eduardo Melgarejo <edumelgarejo27@gmail.com>
2023-06-23 00:49:21 -0300

    Añadido ABM de sensores de basura junto con generación de Eventos y vistas

--- src/main/java/com/unla/grupo4/entities/SensorBasura.java
+++ src/main/java/com/unla/grupo4/entities/SensorBasura.java
@@ -1,15 +1,31 @@
 package com.unla.grupo4.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@PrimaryKeyJoinColumn(name = "id")
 @Getter @Setter @NoArgsConstructor
 public class SensorBasura extends Dispositivo{
 
 	private boolean lleno;
 	
 	private int nroDepartamento;
+	
+	private float distanciaBasura;
+	
+	public SensorBasura(String nombre, boolean activo,boolean lleno, int nroDepartamento, float distanciaBasura) {
+		super(nombre, activo);
+		this.lleno = lleno;
+		this.nroDepartamento = nroDepartamento;
+		this.distanciaBasura = distanciaBasura;
+	}
+	
+	public void reiniciar() {
+		this.setLleno(false);
+		this.setDistanciaBasura(1);
+	}
 }
